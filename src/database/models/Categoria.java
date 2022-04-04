3fbb86dd1435a4d42bb71609d814a17f2577ac81
Lucas Lucena <lucas.lucenak@gmail.com>
2022-04-04 17:48:05 -0300

    added Categoria and Produto classes

---
+++ src/database/models/Categoria.java
@@ -0,0 +1,21 @@
+package database.models;
+
+public class Categoria {
+    private String nome;
+
+    Categoria() {
+
+    }
+
+    public Categoria(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
---
+++ src/database/models/Produto.java
@@ -0,0 +1,41 @@
+package database.models;
+
+public class Produto {
+    private String nome;
+    private String categoria;
+    private Double preco;
+
+    Produto() {
+
+    }
+
+    public Produto(String nome, String categoria, Double preco) {
+        this.nome = nome;
+        this.categoria = categoria;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+}
