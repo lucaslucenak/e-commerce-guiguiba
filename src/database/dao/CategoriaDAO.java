9082962f6eef79faf90668cf859f09c8a23a465a
Lucas Lucena <lucas.lucenak@gmail.com>
2022-04-04 17:56:05 -0300

    created categoriaDAO

--- src/database/dao/CategoriaDAO.java
+++ src/database/dao/CategoriaDAO.java
@@ -1,4 +1,43 @@
 package database.dao;
 
+import database.factory.ConnectionFactory;
+import database.models.Categoria;
+import database.models.Cliente;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class CategoriaDAO {
+    //CRUD
+
+    public void create(Categoria categoria) throws SQLException {
+        Connection con = null;
+        PreparedStatement pstm = null;
+
+        //Query SQL
+        String sql = "INSERT INTO tb_categorias(nome)" +
+                "VALUES (?)";
+        try {
+            con = ConnectionFactory.getConnection();
+            pstm = con.prepareStatement(sql);
+            pstm.setString(1, categoria.getNome());
+
+            pstm.executeUpdate();
+
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        } finally { //Fecha as conexões caso estejam abertas
+            try {
+                if (pstm != null) {
+                    pstm.close();
+                }
+                if (con != null) {
+                    con.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
